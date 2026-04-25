9587c33144a1d37445ec2f0fab28848b31628be0
Suraj Prakash <16456012+suraj083@users.noreply.github.com>
2026-04-25 19:09:00 +0200

    reorganized the code for benchmarking into a new submodule

--- py/benchmark-MSSM.py
+++ py/benchmark-MSSM.py
@@ -1,10 +1,8 @@
-from operator import call
 from match_to_py import MSSM
-from utils.io import *
-from utils.io.wcxf import wcxf_to_matchete_name
+from utils.io import read_param_values, write_to_wcxf
 from utils.core import exec
-from time import perf_counter
-import wcxf
+from utils.benchmarks import *
+
 
 ###########################
 #   parameter definitions
@@ -50,105 +48,34 @@ print(f"Initialized {len(models)} models in {t4 - t3:.2f}s.")
 scale = {"mubarsq": 1e6, "hbar": 0.006332574}
 eft_info = {"eft": "SMEFT", "basis": "Warsaw"}
 
-###############################################################
-#     Total time for writing all operators, WCxf vs native 
-###############################################################
 
-# the WCxf writer
-# f_name = "./benchmark-points/all_wcxf.yaml"
-# t_i = perf_counter() 
-# write_to_wcxf(f_name,models[0],eft_info,**scale) 
-# t_f = perf_counter()
-# print(f"File written in WCxf format in {t_f - t_i:.2f}s.")
+#################
+#   Benchmarks 
+#################
 
-# the native yaml writer for all operators defined in the WCxf basis
-# smeft_warsaw = wcxf.Basis["SMEFT","Warsaw"]
-# wcs_wcxf = smeft_warsaw.all_wcs
-# wc_names: list[str] = list()
-# for wc in wcs_wcxf:
-#     name = wc.split("_")
-#     new_name = wcxf_to_matchete_name(name[0])
-#     if len(name) == 1:
-#         wc_names.append(new_name)
-#     else:
-#         wc_names.append(new_name + "_" + name[1])   
+# Writing all operators, WCxf (seq) vs native yaml (seq) 
+# wcxf_vs_yaml_all_seq(eft_info, models[0], param_dicts[0], **scale)
 
-# f_name = "./benchmark-points/all_native.yaml" 
-# t_i = perf_counter()
-# keys = [[],wc_names]
-# write_to_yaml(f_name,models[0],param_dicts[0],keys,"seq",**scale) 
-# t_f = perf_counter()
-# print(f"File written in native format (using sequential process) in {t_f - t_i:.2f}s.")
+# Writing all operators, native yaml, seq vs par
+# yaml_seq_vs_par_all(eft_info, models[0], param_dicts[0], **scale) 
 
+# Writing 59 operators of the SMEFT-d6 Warsaw basis
+# wcxf_vs_yaml_SMEFTd6_59_seq(models[0], param_dicts[0], **scale)
 
-########################################################################
-#     Total time for writing all operators, parallel (C++) native 
-########################################################################
-
-# f_name = "./benchmark-points/all_native_par_c.yaml" 
-# t_i = perf_counter()
-# keys = [[],wc_names]
-# write_to_yaml(f_name,models[0],param_dicts[0],keys,"par",**scale) 
-# t_f = perf_counter()
-# print(f"File written in native format (using parallel C++ process) in {t_f - t_i:.2f}s.")
-
-
-# ###############################################################
-#     Total time for writing 59 operators, WCxf vs native 
-# ###############################################################
-
-# # 59 Wilson coefficients in WCxf format
-# wc_names_wcxf = ["G", "W", "phi", "phiBox", "phiD", "phiG", "phiB", "phiW", "phiWB", "ephi_33", "dphi_33", "uphi_33", "eB_33", "eW_33", "dB_33", "dW_33", "dG_33", "uB_33", "uW_33", "uG_33", "phie_33", "phiu_33", "phid_33", "phiud_33","phil1_33", "phil3_33", "phiq1_33", "phiq3_33", "ll_3333", "qq1_3333", "qq3_3333", "lq1_3333", "lq3_3333", "ee_3333", "uu_3333", "dd_3333", "eu_3333", "ed_3333", "ld_3333", "lu_3333", "qe_3333", "qu1_3333", "qu8_3333", "qd1_3333", "qd8_3333", "ud1_3333", "ud8_3333", "quqd1_3333", "quqd8_3333", "ledq_3333", "lequ1_3333", "lequ3_3333"]
-
-# f_name = "./benchmark-points/59_wcxf.yaml"
-# t_i = perf_counter() 
-# write_to_wcxf(f_name,models[0],eft_info,wc_names_wcxf,**scale)
-# t_f = perf_counter()
-# print(f"File written in WCxf format in {t_f - t_i:.2f}s.")
-
-# # 59 Wilson coefficients in Matchete format
-# wc_names_matchete = ["cG", "cW", "cHBox", "cH", "cHD", "cHG", "cHB", "cHW", "cHWB", "ceH_33", "cdH_33", "cuH_33", "ceB_33", "ceW_33", "cdB_33", "cdW_33", "cdG_33", "cuB_33", "cuW_33", "cuG_33", "cHe_33", "cHu_33", "cHd_33", "cHud_33","cHl1_33", "cHl3_33", "cHq1_33", "cHq3_33", "cll_3333", "cqq1_3333", "cqq3_3333", "clq1_3333", "clq3_3333", "cee_3333", "cuu_3333", "cdd_3333", "ceu_3333", "ced_3333", "cld_3333", "clu_3333", "cqe_3333", "cqu1_3333", "cqu8_3333", "cqd1_3333", "cqd8_3333", "cud1_3333", "cud8_3333", "cquqd1_3333", "cquqd8_3333", "cledq_3333", "clequ1_3333", "clequ3_3333"] 
-
-# f_name = "./benchmark-points/59_native.yaml" 
-# t_i = perf_counter()
-# keys = [[],wc_names_matchete]
-# write_to_yaml(f_name,models[0],param_dicts[0],keys,"seq",**scale) 
-# t_f = perf_counter()
-# print(f"File written in native format in {t_f - t_i:.2f}s.")
-
-
-#############################################
-#    eval_wc() vs direct method call
-#############################################
-
+# Single coefficient execution times
 # exec(models[0], "cH", **scale)
 # exec(models[0], "cuH_33", **scale)
 # exec(models[0], "cqd1_2213", **scale)
-
-# t_i = perf_counter()
-# x = models[0].cH(**scale)
-# t_f = perf_counter()
-# print(f"{x}, {t_f-t_i}s")
-
-# t_i = perf_counter()
-# x = models[0].cuH(2,2,**scale)
-# t_f = perf_counter()
-# print(f"{x}, {t_f-t_i}s")
-
-# t_i = perf_counter()
-# x = models[0].cqd1(1,1,0,2,**scale)
-# t_f = perf_counter()
-# print(f"{x}, {t_f-t_i}s")
 
 
 ####################
 #   File Writing 
 ####################
 
-# print("Writing to files.")
-# for i in range(5):
-#     t_i = perf_counter()
-#     f_name = "./benchmark-points/wcxf_all_" + str(i+1) + ".yaml" 
-#     write_to_wcxf_all(f_name,models[i],**scale) 
-#     t_f = perf_counter()
-#     print(f"File {i+1} written in {t_f - t_i:.2f}s.")
+print("Writing to files.")
+for i in range(5):
+    t_i = perf_counter()
+    f_name = "./benchmark-points/wcxf_all_" + str(i+1) + ".yaml" 
+    write_to_wcxf(f_name,models[i],eft_info,**scale) 
+    t_f = perf_counter()
+    print(f"File {i+1} written in {t_f - t_i:.2f}s.")
---
+++ py/utils/benchmarks.py
@@ -0,0 +1,99 @@
+from utils.io import write_to_wcxf, write_to_yaml
+from utils.io.wcxf import wcxf_to_matchete_name
+from time import perf_counter
+import wcxf
+
+
+def wcxf_vs_yaml_all_seq(eft_info, model, param_dict, **kw) -> None:
+    """
+        Compares the execution times for evaluating and writing to file all Wilson coefficients
+        defined within a specifc EFT-basis in (i) native yaml (ii) WCxf format
+    """
+    
+    # the WCxf writer
+    f_name = "./benchmark-points/all_wcxf.yaml"
+    t_i = perf_counter() 
+    write_to_wcxf(f_name,model,eft_info,**kw) 
+    t_f = perf_counter()
+    print(f"File written in WCxf format (using sequential process) in {t_f - t_i:.2f}s.")
+
+    # the native yaml writer for all operators defined in the WCxf basis
+    eft_basis = wcxf.Basis[eft_info["eft"], eft_info["basis"]]
+    wcs_wcxf = eft_basis.all_wcs
+    wc_names: list[str] = list()
+    for wc in wcs_wcxf:
+        name = wc.split("_")
+        new_name = wcxf_to_matchete_name(name[0])
+        if len(name) == 1:
+            wc_names.append(new_name)
+        else:
+            wc_names.append(new_name + "_" + name[1])   
+
+    f_name = "./benchmark-points/all_native.yaml" 
+    t_i = perf_counter()
+    keys = [[],wc_names]
+    write_to_yaml(f_name,model,param_dict,keys,"seq",**kw) 
+    t_f = perf_counter()
+    print(f"File written in native format (using sequential process) in {t_f - t_i:.2f}s.")
+
+
+def wcxf_vs_yaml_SMEFTd6_59_seq(model, param_dict, **kw) -> None:
+    """
+        Compares the execution times for evaluating and writing to file the 59 Wilson coefficients
+        of (single fermion generation) dimension 6 SMEFT Warsaw basis (i) native yaml (ii) WCxf format
+    """
+    
+    # 59 Wilson coefficients in WCxf format
+    wc_names_wcxf = ["G", "W", "phi", "phiBox", "phiD", "phiG", "phiB", "phiW", "phiWB", "ephi_33", "dphi_33", "uphi_33", "eB_33", "eW_33", "dB_33", "dW_33", "dG_33", "uB_33", "uW_33", "uG_33", "phie_33", "phiu_33", "phid_33", "phiud_33","phil1_33", "phil3_33", "phiq1_33", "phiq3_33", "ll_3333", "qq1_3333", "qq3_3333", "lq1_3333", "lq3_3333", "ee_3333", "uu_3333", "dd_3333", "eu_3333", "ed_3333", "ld_3333", "lu_3333", "qe_3333", "qu1_3333", "qu8_3333", "qd1_3333", "qd8_3333", "ud1_3333", "ud8_3333", "quqd1_3333", "quqd8_3333", "ledq_3333", "lequ1_3333", "lequ3_3333"]
+
+    
+    eft_info = {"eft": "SMEFT", "basis": "Warsaw"}
+    
+    f_name = "./benchmark-points/59_wcxf.yaml"
+    t_i = perf_counter() 
+    write_to_wcxf(f_name,model,eft_info,wc_names_wcxf,**kw)
+    t_f = perf_counter()
+    print(f"File written in WCxf format in {t_f - t_i:.2f}s.")
+
+    # 59 Wilson coefficients in Matchete format
+    wc_names_matchete = ["cG", "cW", "cHBox", "cH", "cHD", "cHG", "cHB", "cHW", "cHWB", "ceH_33", "cdH_33", "cuH_33", "ceB_33", "ceW_33", "cdB_33", "cdW_33", "cdG_33", "cuB_33", "cuW_33", "cuG_33", "cHe_33", "cHu_33", "cHd_33", "cHud_33","cHl1_33", "cHl3_33", "cHq1_33", "cHq3_33", "cll_3333", "cqq1_3333", "cqq3_3333", "clq1_3333", "clq3_3333", "cee_3333", "cuu_3333", "cdd_3333", "ceu_3333", "ced_3333", "cld_3333", "clu_3333", "cqe_3333", "cqu1_3333", "cqu8_3333", "cqd1_3333", "cqd8_3333", "cud1_3333", "cud8_3333", "cquqd1_3333", "cquqd8_3333", "cledq_3333", "clequ1_3333", "clequ3_3333"] 
+
+    f_name = "./benchmark-points/59_native.yaml" 
+    t_i = perf_counter()
+    keys = [[],wc_names_matchete]
+    write_to_yaml(f_name,model,param_dict,keys,"seq",**kw) 
+    t_f = perf_counter()
+    print(f"File written in native format in {t_f - t_i:.2f}s.")
+
+
+def yaml_seq_vs_par_all(eft_info, model, param_dict, **kw) -> None:
+    """
+        Compares the execution times for evaluating and writing to file all Wilson coefficients
+        defined within a specifc EFT-basis in the native yaml format for (i) sequential and 
+        (ii) parallel execution
+    """
+    
+    eft_basis = wcxf.Basis[eft_info["eft"], eft_info["basis"]]
+    wcs_wcxf = eft_basis.all_wcs
+    wc_names: list[str] = list()
+    for wc in wcs_wcxf:
+        name = wc.split("_")
+        new_name = wcxf_to_matchete_name(name[0])
+        if len(name) == 1:
+            wc_names.append(new_name)
+        else:
+            wc_names.append(new_name + "_" + name[1])   
+
+    f_name = "./benchmark-points/all_native.yaml" 
+    t_i = perf_counter()
+    keys = [[],wc_names]
+    write_to_yaml(f_name,model,param_dict,keys,"seq",**kw) 
+    t_f = perf_counter()
+    print(f"File written in native format (using sequential process) in {t_f - t_i:.2f}s.")
+
+    f_name = "./benchmark-points/all_native_par.yaml" 
+    t_i = perf_counter()
+    keys = [[],wc_names]
+    write_to_yaml(f_name,model,param_dict,keys,"par",**kw) 
+    t_f = perf_counter()
+    print(f"File written in native format (using parallel C++ process) in {t_f - t_i:.2f}s.")
