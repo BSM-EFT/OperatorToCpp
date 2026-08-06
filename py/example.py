8518339f76acc8e7f014eec7f0cd1a3bb1239a33
Suraj Prakash <16456012+suraj083@users.noreply.github.com>
2026-08-06 17:30:33 +0200

    set very heavy masses to the same large value

--- py/example.py
+++ py/example.py
@@ -3,15 +3,11 @@ from utils.io import write_to_wcxf
 
 # define a parameter dictionary
 param_dict = {
-    "g1": 0.37, "g3": 1.1, "cgamma": 0.01,
-    "Yu11": 0.00001, "Yu22": 0.007, "Yu33": 0.9,
-    "mPhi": 1001000, "muTilde": 1000000,
-    "m1": 1.2, "m2": 1002000, "m3": 1003000,
-    "met1": 1004000, "met2": 1005000, "met3": 1006000,
-    "mlt1": 1007000, "mlt2": 1008000, "mlt3": 1009000,
-    "mqt1": 1010000, "mqt2": 1011000, "mqt3": 1012000,
-    "mut1": 1013000, "mut2": 1014000, "mut3": 2.0,
-    "mdt1": 1016000, "mdt2": 1017000, "mdt3": 1018000,
+    "g1": 0.37, "g3": 1.1, "cgamma": 0.01, "Yu11": 0.00001, "Yu22": 0.007, "Yu33": 0.9,
+    "met1": 1e6, "met2": 1e6, "met3": 1e6, "mlt1": 1e6, "mlt2": 1e6, "mlt3": 1e6,
+    "mqt1": 1e6, "mqt2": 1e6, "mqt3": 1e6, "mut1": 1e6, "mut2": 1e6, "mut3": 2.0,
+    "mdt1": 1e6, "mdt2": 1e6, "mdt3": 1e6, "m1": 1.2, "m2": 1e6, "m3": 1e6,
+    "mPhi": 1e6, "muTilde": 1e6
 }
 
 # create an instance of the MSSM class
