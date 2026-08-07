d4f9f2da456870b0cd54bb9d0c123ffff9b1d512
Suraj Prakash <16456012+suraj083@users.noreply.github.com>
2026-08-07 07:02:30 +0200

    simplified benchmark-MSSM.py

--- Examples/MSSM-to-SMEFTd6/py/benchmark-MSSM.py
+++ Examples/MSSM-to-SMEFTd6/py/benchmark-MSSM.py
@@ -1,52 +1,26 @@
 from match_to_py import MSSM
-from utils.io import read_param_values, write_to_wcxf
 from utils.core import exec
 from utils.benchmarks import *
-from time import perf_counter
 
 
 ###########################
 #   parameter definitions
 ###########################
 
-# SM parameters (common to all benchmark points)
-param_dict_sm = {
-    "g1": 0.354474, "g2": 0.639861, "g3": 1.06197,
-    "Ye11": 2.93503e-6, "Ye22": 0.00060688, "Ye33": 0.0102062,
-    "Yd11": 0.0000269954, "Yd22": 0.000537037, "Yd33": 0.0240259,
-    "Yu11": 0.0000120923, "Yu12": -0.00164521, "Yu13": 0.00683461 + 0.00295002j,
-    "Yu21": 2.79157e-6, "Yu22": 0.00712664, "Yu23": -0.0362955,
-    "Yu31": 1.9044e-8 + 4.21698e-8j, "Yu32": 0.000305777,  "Yu33": 0.867899,
+param_dict1 = {
+    "g1": 0.37, "g3": 1.1, "cgamma": 0.01, "Yu11": 0.00001, "Yu22": 0.007, "Yu33": 0.9,
+    "met1": 1e9, "met2": 1e9, "met3": 1e9, "mlt1": 1e9, "mlt2": 1e9, "mlt3": 1e9,
+    "mqt1": 1e9, "mqt2": 1e9, "mqt3": 1e9, "mut1": 1e9, "mut2": 1e9, "mut3": 2e3,
+    "mdt1": 1e9, "mdt2": 1e9, "mdt3": 1e9, "m1": 1.2e3, "m2": 1e9, "m3": 1e9,
+    "mPhi": 1e9, "muTilde": 1e9
 }
-
-print("Loading data from file.")
-t1 = perf_counter()
-
-param_dicts = list()
-for i in range(1,6):
-    filename = "./benchmark-points/bp_" + str(i) + ".yaml"
-    par_dict_mssm, _ = read_param_values(filename)
-    par_dict = param_dict_sm | par_dict_mssm
-    param_dicts.append(par_dict)
-
-t2 = perf_counter()
-print(f"Data for {len(param_dicts)} benchmark points loaded in {t2 - t1:.2f}s.")
 
 
 ###########################
 #   Model Initialization
 ###########################
 
-print("Initializing MSSM model instances.")
-t3 = perf_counter()
-models = list()
-for p_dict in param_dicts:
-    model = MSSM(p_dict, 1e3, True)
-    models.append(model)
-
-t4 = perf_counter()
-print(f"Initialized {len(models)} models in {t4 - t3:.2f}s.")
-
+model1 = MSSM(param_dict1, 1e3, True)
 eft_info = {"eft": "SMEFT", "basis": "Warsaw"}
 
 
@@ -55,35 +29,22 @@ eft_info = {"eft": "SMEFT", "basis": "Warsaw"}
 #################
 
 # Writing all operators, WCxf (seq) vs native yaml (seq)
-wcxf_vs_yaml_all_seq(eft_info, models[0], param_dicts[0])
+wcxf_vs_yaml_all_seq(eft_info, model1, param_dict1)
 
 # Writing all operators, WCxf seq vs par
-wcxf_seq_vs_par_all(eft_info, models[0])
+wcxf_seq_vs_par_all(eft_info, model1)
 
 # Writing all operators, native yaml, seq vs par
-yaml_seq_vs_par_all(eft_info, models[0], param_dicts[0])
+yaml_seq_vs_par_all(eft_info, model1, param_dict1)
 
 # Writing 59 operators of the SMEFT-d6 Warsaw basis, sequentially
-wcxf_vs_yaml_SMEFTd6_59_seq(models[0], param_dicts[0])
+wcxf_vs_yaml_SMEFTd6_59_seq(model1, param_dict1)
 
 # Writing 59 operators of the SMEFT-d6 Warsaw basis, in parallel
-wcxf_vs_yaml_SMEFTd6_59_par(models[0], param_dicts[0])
+wcxf_vs_yaml_SMEFTd6_59_par(model1, param_dict1)
 
 
 # Single coefficient execution times
-exec(models[0], "cH")
-exec(models[0], "cuH_33")
-exec(models[0], "cqd1_2213")
-
-
-####################
-#   File Writing
-####################
-
-print("Writing to files.")
-for i in range(5):
-    t_i = perf_counter()
-    f_name = "./benchmark-points/wcxf_all_" + str(i+1) + ".yaml"
-    write_to_wcxf(f_name,models[i],eft_info)
-    t_f = perf_counter()
-    print(f"File {i+1} written in {t_f - t_i:.2f}s.")
+exec(model1, "cH")
+exec(model1, "cuH_33")
+exec(model1, "cqd1_2213")
